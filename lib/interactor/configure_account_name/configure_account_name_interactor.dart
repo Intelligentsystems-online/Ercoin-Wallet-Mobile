9cbbf3925ff85224dbe9cf80fbc01212ab9160c7
mpomykalski <mateusz.pomykalski@wext.it>
2019-11-26 19:34:25 +0100

    create implementation for addAccount in ConfigureAccountNameInteractor

--- lib/interactor/configure_account_name/configure_account_name_interactor.dart
+++ lib/interactor/configure_account_name/configure_account_name_interactor.dart
@@ -1,10 +1,18 @@
 import 'dart:async';
 
+import 'package:convert/convert.dart';
+import 'package:ercoin_wallet/repository/account/Account.dart';
+import 'package:ercoin_wallet/repository/account/AccountRepository.dart';
 import 'package:flutter_sodium/flutter_sodium.dart';
 
-// TODO(Interactor)
 class ConfigureAccountNameInteractor {
-  Future<void> addAccount(KeyPair keyPair, String name) async {
-    return;
-  }
+  final _accountRepository = AccountRepository();
+  
+  Future<void> addAccount(KeyPair keyPair, String name) async => _accountRepository
+      .createAccount(_prepareAccountFor(keyPair, name));
+
+  _prepareAccountFor(KeyPair keyPair, String name) => Account(
+    hex.encode(keyPair.publicKey),
+    hex.encode(keyPair.secretKey),
+    name);
 }
