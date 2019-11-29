a9b52579378fea4d6e96bf0590ddef88a996f26c
mpomykalski <mateusz.pomykalski@wext.it>
2019-11-29 19:00:34 +0100

    add onAddress pressed listener

--- lib/utils/view/address_list.dart
+++ lib/utils/view/address_list.dart
@@ -1,3 +1,4 @@
+import 'package:ercoin_wallet/repository/account/Account.dart';
 import 'package:ercoin_wallet/repository/address/Address.dart';
 import 'package:ercoin_wallet/utils/view/expanded_row.dart';
 
@@ -5,12 +6,18 @@ import 'package:flutter/cupertino.dart';
 
 class AddressList extends StatelessWidget {
   final List<Address> addresses;
+  final Function(Account) onAddressPressed;
 
-  const AddressList({this.addresses});
+  const AddressList({this.addresses, this.onAddressPressed});
 
   @override
   Widget build(BuildContext context) => ListView.builder(
       itemCount: addresses.length,
-      itemBuilder: (context, index) =>  ExpandedRow(child: Text(addresses[index].accountName))
+      itemBuilder: (context, index) => ExpandedRow(child: _prepareAddressRow(addresses[index]))
+  );
+
+  Widget _prepareAddressRow(Address address) => GestureDetector(
+    onTap: () => onAddressPressed,
+    child: Text(address.accountName)
   );
 }
