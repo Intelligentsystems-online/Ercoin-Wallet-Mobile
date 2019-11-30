5ed2e306efb01f899fbab8ceaca891032d472cce
mpomykalski <mateusz.pomykalski@wext.it>
2019-11-30 20:54:30 +0100

    add shrinkWrap: true property in transaction List builder

--- lib/utils/view/transaction_list.dart
+++ lib/utils/view/transaction_list.dart
@@ -11,6 +11,7 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ListView.builder(
+      shrinkWrap: true,
       itemCount: transactions.length,
       itemBuilder: (context, index) => _transactionRow(transactions[index])
   );
