07f74c0eabf77ce906a1d6a18e53e5735e9c3ea9
mpomykalski <mateusz.pomykalski@wext.it>
2019-11-30 17:38:19 +0100

    add recentTransactions method in HomeInteractor

--- lib/interactor/home/home_interactor.dart
+++ lib/interactor/home/home_interactor.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 
+import 'package:ercoin_wallet/model/Transaction.dart';
 import 'package:ercoin_wallet/model/account_with_balance.dart';
 import 'package:ercoin_wallet/repository/account/Account.dart';
 
 // TODO(Interactor)
 class HomeInteractor {
-  Future<AccountWithBalance> activeAccountWithBalance() {
-    return Future.value(AccountWithBalance(
-        Account("public key", "private key", "account name"),
-        0));
+  Future<AccountWithBalance> activeAccountWithBalance() async {
+    return AccountWithBalance(Account("public key", "private key", "account name"), 0);
+  }
+
+  Future<List<Transaction>> recentTransactions() async {
+    var transaction = Transaction("receiver address", "sender address", 100, "message", 57000);
+
+    return [transaction];
   }
 }
