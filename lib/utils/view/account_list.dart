10833e2384d46768df66417ab9bbbf0731c461d5
mpomykalski <mateusz.pomykalski@wext.it>
2019-11-30 12:35:43 +0100

    create model AccountWithBalance for account list widget

--- lib/utils/view/account_list.dart
+++ lib/utils/view/account_list.dart
@@ -1,24 +1,28 @@
+import 'package:ercoin_wallet/model/account_with_balance.dart';
 import 'package:ercoin_wallet/repository/account/Account.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AccountList extends StatelessWidget {
-  final List<Account> accounts;
+  final List<AccountWithBalance> accountsWithBalance;
   final String activeAccountPk;
-  final Function(Account) onAccountPressed;
+  final Function(AccountWithBalance) onAccountPressed;
 
-  const AccountList({this.accounts, this.activeAccountPk, this.onAccountPressed});
+  const AccountList({this.accountsWithBalance, this.activeAccountPk, this.onAccountPressed});
 
   @override
   Widget build(BuildContext context) => ListView.builder(
-      itemCount: accounts.length,
-      itemBuilder: (context, index) => _accountBox(accounts[index])
+      itemCount: accountsWithBalance.length,
+      itemBuilder: (context, index) => _accountBox(accountsWithBalance[index])
   );
 
-  Widget _accountBox(Account account) => GestureDetector(
-    onTap: onAccountPressed(account),
+  Widget _accountBox(AccountWithBalance accountWithBalance) => GestureDetector(
+    onTap: onAccountPressed(accountWithBalance),
     child: Row(
-      children: <Widget>[_activeAccountSign(account), _accountLabel(account)],
+      children: <Widget>[
+        _activeAccountSign(accountWithBalance.account),
+        _accountLabel(accountWithBalance.account),
+        Text(accountWithBalance.balance.toString())],
     )
   );
 
