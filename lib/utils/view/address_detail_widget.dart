c048a264bac8640edbd89fc9677828a84e37e404
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-01 17:39:12 +0100

    move address detail to address_detail_widget file in view/utils

---
+++ lib/utils/view/address_detail_widget.dart
@@ -0,0 +1,26 @@
+import 'package:ercoin_wallet/repository/address/Address.dart';
+import 'package:ercoin_wallet/utils/view/expanded_row.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddressDetailWidget extends StatelessWidget {
+  final Address address;
+
+  const AddressDetailWidget(this.address);
+
+  @override
+  Widget build(BuildContext ctx) => Column(
+    children: <Widget>[
+      Text("Name: " + address.accountName),
+      Text("Address: " + address.publicKey),
+      _closeBtn(ctx)
+    ]
+  );
+
+  ExpandedRow _closeBtn(BuildContext ctx) => ExpandedRow(
+    child: FlatButton(
+      child: Text("Close", style: TextStyle(color: Colors.blueAccent, fontSize: 16)),
+      onPressed: () => Navigator.of(ctx).pop(),
+    )
+  );
+}
--- lib/view/address_list/address_list_page.dart
+++ lib/view/address_list/address_list_page.dart
@@ -1,7 +1,7 @@
 import 'package:ercoin_wallet/interactor/address_list/AddressListInteractor.dart';
 import 'package:ercoin_wallet/repository/address/Address.dart';
+import 'package:ercoin_wallet/utils/view/address_detail_widget.dart';
 import 'package:ercoin_wallet/utils/view/address_list.dart';
-import 'package:ercoin_wallet/utils/view/expanded_row.dart';
 import 'package:ercoin_wallet/utils/view/future_builder_with_progress.dart';
 
 import 'package:flutter/cupertino.dart';
@@ -28,19 +28,6 @@ class AddressListPage extends StatelessWidget {
 
   AlertDialog _prepareAlertDialog(BuildContext ctx, Address address) => AlertDialog(
       title: Center(child: Text("Address detail")),
-      content: _addressDetail(ctx, address)
-  );
-
-  Widget _addressDetail(BuildContext ctx, Address address) => Column(
-    children: <Widget>[
-      Text("Name: " + address.accountName),
-      Text("Address: " + address.publicKey),
-      ExpandedRow(
-        child: FlatButton(
-          child: Text("Close", style: TextStyle(color: Colors.blueAccent, fontSize: 16)),
-          onPressed: () => Navigator.of(ctx).pop(),
-        ),
-      )
-    ],
+      content: AddressDetailWidget(address)
   );
 }
