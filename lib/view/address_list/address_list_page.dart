4b097cfe5d840b5c69bde1a264cbfb56f12a1d8b
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-02 13:21:13 +0100

    add addAddress button

--- lib/view/address_list/address_list_page.dart
+++ lib/view/address_list/address_list_page.dart
@@ -3,6 +3,9 @@ import 'package:ercoin_wallet/repository/address/Address.dart';
 import 'package:ercoin_wallet/utils/view/address_detail_widget.dart';
 import 'package:ercoin_wallet/utils/view/address_list.dart';
 import 'package:ercoin_wallet/utils/view/future_builder_with_progress.dart';
+import 'package:ercoin_wallet/utils/view/navigation_utils.dart';
+import 'package:ercoin_wallet/utils/view/values.dart';
+import 'package:ercoin_wallet/view/createAddress/CreateAddressScreen.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -11,14 +14,27 @@ class AddressListPage extends StatelessWidget {
   final _interactor = AddressListInteractor(); //TODO(DI)
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext ctx) => Scaffold(
     appBar: AppBar(
       title: Text("Addresses"),
     ),
-    body: FutureBuilderWithProgress(
-      future: _interactor.obtainAddresses(),
-      builder: (List<Address> addresses) => AddressList(addresses, (address) => _onAddressPressed(context, address))
+    body: Stack(
+      children: <Widget>[
+        Align(
+          alignment: FractionalOffset.topCenter,
+          child: _addressListBuilder(ctx)
+        ),
+        Align(
+          alignment: FractionalOffset.bottomRight,
+          child: _addAddressBtn(ctx)
+        )
+      ],
     )
+  );
+
+  Widget _addressListBuilder(BuildContext ctx) => FutureBuilderWithProgress(
+      future: _interactor.obtainAddresses(),
+      builder: (List<Address> addresses) => AddressList(addresses, (address) => _onAddressPressed(ctx, address))
   );
 
   _onAddressPressed(BuildContext ctx, Address address) => showDialog(
@@ -29,5 +45,17 @@ class AddressListPage extends StatelessWidget {
   AlertDialog _prepareAlertDialog(BuildContext ctx, Address address) => AlertDialog(
       title: Center(child: Text("Address detail")),
       content: AddressDetailWidget(address)
+  );
+
+  Widget _addAddressBtn(BuildContext ctx) => RawMaterialButton(
+    shape: CircleBorder(),
+    padding: standardPadding,
+    fillColor: Colors.white,
+    child: Icon(Icons.add, color: Colors.blue, size: 35.0),
+    onPressed: () => _navigateToAddAddress(ctx),
+  );
+
+  _navigateToAddAddress(BuildContext ctx) => pushRoute(
+      Navigator.of(ctx), () => CreateAddressScreen() //TODO change to add_address_route after merge.
   );
 }
