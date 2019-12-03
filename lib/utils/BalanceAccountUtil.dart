a76b356721b819efdfc65ef839a8f7960ef3cb12
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-03 20:59:16 +0100

    rid of micro prefix

--- lib/utils/BalanceAccountUtil.dart
+++ lib/utils/BalanceAccountUtil.dart
@@ -5,11 +5,14 @@ class BalanceAccountUtil
 {
   double obtainBalanceValue(Uint8List accountDataBytes) {
     ByteBuffer buffer = _balanceBytesFrom(accountDataBytes).buffer;
-
-    return ByteData
+    int microErcoinAmount = ByteData
         .view(buffer)
-        .getInt64(0) / 1000000;
+        .getInt64(0);
+
+    return _ridOfMicroPrefixFrom(microErcoinAmount);
   }
 
   Uint8List _balanceBytesFrom(Uint8List accountDataBytes) => accountDataBytes.sublist(4, 12);
+
+  double _ridOfMicroPrefixFrom(int amount) => amount / 1000000;
 }
