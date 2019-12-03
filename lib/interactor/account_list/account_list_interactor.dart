9d9cd84847d29b2ba3689d8332149abda74b7e42
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-03 18:43:23 +0100

    account list interactor: remove rxdart and use Future.wait(_) against rx

--- lib/interactor/account_list/account_list_interactor.dart
+++ lib/interactor/account_list/account_list_interactor.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:ercoin_wallet/utils/SharedPreferencesUtil.dart';
-import 'package:rxdart/rxdart.dart';
 
 import 'package:ercoin_wallet/model/account_with_balance.dart';
 import 'package:ercoin_wallet/repository/account/Account.dart';
@@ -19,18 +18,16 @@ class AccountListInteractor {
 
   Future<List<AccountWithBalance>> obtainAccountsWithBalance() async {
     final accounts = await _accountRepository.findAll();
+    final futureAccounts = accounts.map((account) => toAccountWithBalance(account));
 
-    return Observable
-        .fromIterable(accounts)
-        .flatMap((account) => toAccountWithBalance(account))
-        .toList();
+    return Future.wait(futureAccounts);
   }
 
-  Stream<AccountWithBalance> toAccountWithBalance(Account account) async* {
+  Future<AccountWithBalance> toAccountWithBalance(Account account) async {
     final accountDataBase64 = await _apiConsumer.fetchAccountDataBase64For(account.publicKey);
     final accountBalance = _balanceAccountUtil.obtainBalanceValue(base64.decode(accountDataBase64));
 
-    yield AccountWithBalance(account, accountBalance);
+    return AccountWithBalance(account, accountBalance);
   }
 
   Future<String> obtainActiveAccountPk() => _sharedPreferencesUtil.getSharedPreference("active_account");
