705d945e0875677286882c03a60c5a1ed3c8cc07
Jakub Walusiak <jakub.walusiak@proexe.pl>
2019-12-03 19:06:02 +0100

    Complete transfer flow

--- lib/view/home/page/account_info/account_info_page.dart
+++ lib/view/home/page/account_info/account_info_page.dart
@@ -1,10 +1,13 @@
 import 'package:ercoin_wallet/interactor/account_info/account_info_interctor.dart';
 import 'package:ercoin_wallet/model/Transaction.dart';
 import 'package:ercoin_wallet/model/account_with_balance.dart';
+import 'package:ercoin_wallet/utils/view/expanded_raised_text_button.dart';
 import 'package:ercoin_wallet/utils/view/future_builder_with_progress.dart';
+import 'package:ercoin_wallet/utils/view/navigation_utils.dart';
 import 'package:ercoin_wallet/utils/view/transaction_details_widget.dart';
 import 'package:ercoin_wallet/utils/view/transaction_list.dart';
 import 'package:ercoin_wallet/utils/view/values.dart';
+import 'package:ercoin_wallet/view/transfer/select_destination/select_transfer_destination_route.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,30 +18,32 @@ class AccountInfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => FutureBuilderWithProgress(
       future: _interactor.obtainActiveAccountWithBalance(),
-      builder: (AccountWithBalance accountWithBalance) => _accountInfoView(context, accountWithBalance)
-  );
+      builder: (AccountWithBalance accountWithBalance) => _accountInfoView(context, accountWithBalance));
 
   Widget _accountInfoView(BuildContext ctx, AccountWithBalance accountWithBalance) => Container(
-    padding: standardPadding,
-    child: Column(
-      children: <Widget>[
-        Text("Account: " + accountWithBalance.account.accountName),
-        Text("Balance: " + accountWithBalance.balance.toString() + " MICRO ERCOIN"),
-        FutureBuilderWithProgress(
-            future: _interactor.obtainRecentTransactions(),
-            builder: (List<Transaction> transactions) => TransactionList(transactions, (transaction) => _onTransactionPressed(ctx, transaction))
-        )
-      ],
-    ),
-  );
+        padding: standardPadding,
+        child: Column(
+          children: <Widget>[
+            Text("Account: " + accountWithBalance.account.accountName),
+            Text("Balance: " + accountWithBalance.balance.toString() + " MICRO ERCOIN"),
+            FutureBuilderWithProgress(
+              future: _interactor.obtainRecentTransactions(),
+              builder: (transactions) => TransactionList(
+                transactions: transactions,
+                onTransactionPressed: (transaction) => _onTransactionPressed(ctx, transaction),
+              ),
+            ),
+            ExpandedRaisedTextButton(
+              text: "Transfer",
+              onPressed: () => pushRoute(Navigator.of(ctx), () => SelectTransferDestinationRoute()),
+            )
+          ],
+        ),
+      );
 
-  _onTransactionPressed(BuildContext ctx, Transaction transaction) => showDialog(
-      context: ctx,
-      builder: (ctx) => prepareAlertDialog(ctx, transaction)
-  );
+  _onTransactionPressed(BuildContext ctx, Transaction transaction) =>
+      showDialog(context: ctx, builder: (ctx) => prepareAlertDialog(ctx, transaction));
 
-  AlertDialog prepareAlertDialog(BuildContext ctx, Transaction transaction) => AlertDialog(
-      title: Center(child: Text("Transaction detail")),
-      content: TransactionDetailsWidget(transaction)
-  );
+  AlertDialog prepareAlertDialog(BuildContext ctx, Transaction transaction) =>
+      AlertDialog(title: Center(child: Text("Transaction detail")), content: TransactionDetailsWidget(transaction));
 }
---
+++ lib/view/transfer/select_destination/select_transfer_destination_route.dart
@@ -0,0 +1,50 @@
+import 'package:ercoin_wallet/interactor/transfer/select_destination/select_transfer_destination_interactor.dart';
+import 'package:ercoin_wallet/utils/view/address_list.dart';
+import 'package:ercoin_wallet/utils/view/expanded_raised_text_button.dart';
+import 'package:ercoin_wallet/utils/view/future_builder_with_progress.dart';
+import 'package:ercoin_wallet/utils/view/navigation_utils.dart';
+import 'package:ercoin_wallet/utils/view/top_and_bottom_container.dart';
+import 'package:ercoin_wallet/view/enter_address/enter_address_route.dart';
+import 'package:ercoin_wallet/view/transfer/transfer_route.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SelectTransferDestinationRoute extends StatelessWidget {
+  final _interactor = SelectTransferDestinationInteractor(); // TODO(DI)
+
+  @override
+  Widget build(BuildContext ctx) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Select address"),
+        ),
+        body: FutureBuilderWithProgress(
+          future: _interactor.obtainAddressList(),
+          builder: (addresses) => TopAndBottomContainer(
+            top: AddressList(
+              addresses: addresses,
+              onAddressPressed: (ctx, address) => _selectDestination(ctx, address.publicKey, address.accountName),
+            ),
+            bottom: ExpandedRaisedTextButton(
+              text: "New address",
+              onPressed: () => _newAddress(ctx),
+            ),
+          ),
+        ),
+      );
+
+  _selectDestination(BuildContext ctx, String address, [String name]) =>
+      pushRoute(Navigator.of(ctx), () => TransferRoute(destinationAddress: address, destinationName: name));
+
+  _newAddress(BuildContext ctx) => pushRoute(
+        Navigator.of(ctx),
+        () => EnterAddressRoute(
+          isNameOptional: true,
+          onProceed: (ctx, address, name) {
+            if (name != null) {
+              _interactor.addAddress(address, name);
+            }
+            _selectDestination(ctx, address, name);
+          },
+        ),
+      );
+}
