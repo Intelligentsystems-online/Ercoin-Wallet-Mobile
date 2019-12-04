b4016dfd23828d641a739db7b4c8a39dac8387ce
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-04 12:08:48 +0100

    create transaction list service

--- lib/interactor/account_info/account_info_interctor.dart
+++ lib/interactor/account_info/account_info_interctor.dart
@@ -1,48 +1,28 @@
 import 'dart:async';
 
 import 'package:ercoin_wallet/model/Transaction.dart';
-import 'package:ercoin_wallet/model/TransactionFactory.dart';
 import 'package:ercoin_wallet/model/account_with_balance.dart';
-import 'package:ercoin_wallet/utils/ApiConsumer.dart';
-import 'package:ercoin_wallet/utils/SharedPreferencesUtil.dart';
 import 'package:ercoin_wallet/utils/service/account/active_account_service.dart';
+import 'package:ercoin_wallet/utils/service/transaction/list/transaction_list_service.dart';
 
 //TODO(DI)
 class AccountInfoInteractor {
   final _activeAccountService = ActiveAccountService();
-
-  final _sharedPreferencesUtil = SharedPreferencesUtil();
-  final _apiConsumer = ApiConsumer();
-  final _transactionFactory = TransactionFactory();
+  final _transactionListService = TransactionListService();
 
   Future<AccountWithBalance> obtainActiveAccountWithBalance() => _activeAccountService.obtainActiveAccountWithBalance();
 
   Future<List<Transaction>> obtainRecentTransactions() async {
-    final activeAccountPk = await _sharedPreferencesUtil.getSharedPreference("active_account");
-    final outboundTransactionsBase64 = await _apiConsumer.fetchOutboundTransactionBase64ListFor(activeAccountPk);
-    final incomingTransactionsBase64 = await _apiConsumer.fetchIncomingTransactionBase64ListFor(activeAccountPk);
+    final activeAccountPk = await _activeAccountService.obtainActiveAccountPk();
+    final transactions = await _transactionListService.obtainTransactionsFor(activeAccountPk);
 
-    final outboundTransactions = _obtainTransactionsFrom(outboundTransactionsBase64);
-    outboundTransactions.sort((t1, t2) => t2.timestamp.compareTo(t1.timestamp));
-    final incomingTransactions = _obtainTransactionsFrom(incomingTransactionsBase64);
-    incomingTransactions.sort((t1, t2) => t2.timestamp.compareTo(t1.timestamp));
+    transactions.sort(_compareByTimestamp);
 
-    final limitedOutboundTransactions = _obtainLimitedTransactions(3, outboundTransactions);
-    final limitedIncomingTransactions = _obtainLimitedTransactions(3, incomingTransactions);
-
-    final recentTransactions = List<Transaction>();
-
-    recentTransactions.addAll(limitedOutboundTransactions);
-    recentTransactions.addAll(limitedIncomingTransactions);
-    recentTransactions.sort((t1, t2) => t2.timestamp.compareTo(t1.timestamp));
-
-    return recentTransactions;
+    return _obtainLimitedTransactions(5, transactions);
   }
+
+  int _compareByTimestamp(Transaction t1, Transaction t2) => t2.timestamp.compareTo(t1.timestamp);
 
   List<Transaction> _obtainLimitedTransactions(int limit, List<Transaction> transactions) =>
       (transactions.length < limit) ? transactions.sublist(0, transactions.length) : transactions.sublist(0, limit);
-
-  List<Transaction> _obtainTransactionsFrom(List<String> transactionsBase64) => transactionsBase64
-      .map((trxBase64) => _transactionFactory.createFromBase64(trxBase64))
-      .toList();
 }
---
+++ lib/utils/service/transaction/list/transaction_list_service.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:ercoin_wallet/model/Transaction.dart';
+import 'package:ercoin_wallet/utils/ApiConsumer.dart';
+import 'package:ercoin_wallet/model/TransactionFactory.dart';
+
+class TransactionListService {
+  final _transactionFactory = TransactionFactory();
+  final _apiConsumer = ApiConsumer();
+
+  Future<List<Transaction>> obtainOutboundTransactionsFor(String address) async {
+    final transactionsBase64 = await _apiConsumer.fetchOutboundTransactionBase64ListFor(address);
+
+    return _obtainTransactionsFrom(transactionsBase64);
+  }
+
+  Future<List<Transaction>> obtainIncomingTransactionsFor(String address) async {
+    final transactionsBase64 = await _apiConsumer.fetchIncomingTransactionBase64ListFor(address);
+
+    return _obtainTransactionsFrom(transactionsBase64);
+  }
+
+  Future<List<Transaction>> obtainTransactionsFor(String address) async {
+    final incomingTransactions = await obtainIncomingTransactionsFor(address);
+    final outboundTransactions = await obtainOutboundTransactionsFor(address);
+
+    final transactions = List<Transaction>();
+    transactions.addAll(incomingTransactions);
+    transactions.addAll(outboundTransactions);
+
+    return transactions;
+  }
+
+  List<Transaction> _obtainTransactionsFrom(List<String> transactionsBase64) => transactionsBase64
+      .map((trxBase64) => _transactionFactory.createFromBase64(trxBase64))
+      .toList();
+}
