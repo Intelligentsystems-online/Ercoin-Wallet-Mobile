8087d1950f9e33eb95ac299e752ceb4bb7a42c83
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-04 14:04:57 +0100

    move creating uri to uri_factory class

--- lib/utils/service/api/api_consumer.dart
+++ lib/utils/service/api/api_consumer.dart
@@ -1,28 +1,25 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:ercoin_wallet/utils/service/api/uri_factory.dart';
 import 'package:http/http.dart' as http;
-import 'package:convert/convert.dart';
 
 class ApiConsumer
 {
-  static final String _hostname = "testnet-node.ercoin.tech";
-  static final String _fetchTransactionsEndpoint = "/tx_search";
-  static final String _accountDataEndpoint = "/abci_query";
-  static final String _makeTransactionEndpoint = "/broadcast_tx_sync";
+  final uriFactory = UriFactory(); //TODO(DI)
 
   Future<bool> makeTransaction(String transactionHex) => http
-      .get(_prepareMakeTransactionUriFor(transactionHex))
+      .get(uriFactory.makeTransactionUriFor(transactionHex))
       .then((response) => true);
 
   Future<String> fetchAccountDataBase64For(String address) async {
-    final response = await http.get(_prepareAccountDataUriFor(address));
+    final response = await http.get(uriFactory.accountDataUriFor(address));
 
     return jsonDecode(response.body)['result']['response']['value'] as String;
   }
 
   Future<List<String>> fetchOutboundTransactionBase64ListFor(String address) async {
-    final response = await http.get(_prepareOutboundTransactionsUriFor(address));
+    final response = await http.get(uriFactory.outboundTransactionsUriFor(address));
 
     List<dynamic> transactions = jsonDecode(response.body)['result']['txs'];
 
@@ -32,7 +29,7 @@ class ApiConsumer
   }
 
   Future<List<String>> fetchIncomingTransactionBase64ListFor(String address) async {
-    final response = await http.get(_prepareIncomingTransactionsUriFor(address));
+    final response = await http.get(uriFactory.incomingTransactionsUriFor(address));
 
     List<dynamic> transactions = jsonDecode(response.body)['result']['txs'];
 
@@ -40,22 +37,4 @@ class ApiConsumer
         .map((transaction) => transaction['tx'] as String)
         .toList();
   }
-
-  Uri _prepareOutboundTransactionsUriFor(String address) =>
-      new Uri.https(_hostname, _fetchTransactionsEndpoint, { "query" : _prepareOutboundTransactionsQueryValue(address) });
-
-  Uri _prepareIncomingTransactionsUriFor(String address) =>
-      new Uri.https(_hostname, _fetchTransactionsEndpoint, { "query" : _prepareIncomingTransactionsQueryValue(address) });
-
-  String _prepareOutboundTransactionsQueryValue(String address) =>
-      "\"tx.from=" + "'" + base64.encode(hex.decode(address)) + "'\"";
-
-  String _prepareIncomingTransactionsQueryValue(String address) =>
-      "\"tx.to=" + "'" + base64.encode(hex.decode(address)) + "'\"";
-
-  Uri _prepareAccountDataUriFor(String address) =>
-      new Uri.https(_hostname, _accountDataEndpoint, { "path" : "\"account\"", "data" : "0x" + address});
-
-  Uri _prepareMakeTransactionUriFor(String transactionHex) =>
-      new Uri.https(_hostname, _makeTransactionEndpoint, { "tx" : "0x" + transactionHex});
 }
