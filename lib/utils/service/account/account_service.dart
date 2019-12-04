c9e534db1d435b94a98a4d177fb9fd0117988cbf
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-04 12:42:48 +0100

    create generateAccountKeys method in account_service

--- lib/utils/service/account/account_service.dart
+++ lib/utils/service/account/account_service.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 
+import 'package:convert/convert.dart';
+import 'package:ercoin_wallet/model/account_keys.dart';
 import 'package:ercoin_wallet/model/account_with_balance.dart';
 import 'package:ercoin_wallet/repository/account/Account.dart';
 import 'package:ercoin_wallet/repository/account/AccountRepository.dart';
+import 'package:ercoin_wallet/utils/KeyGenerator.dart';
 import 'package:ercoin_wallet/utils/service/account/common_account_util.dart';
+import 'package:flutter_sodium/flutter_sodium.dart';
 
 class AccountService {
   final _commonAccountUtil = CommonAccountUtil();
   final _accountRepository = AccountRepository();
+  final _keyGenerator = KeyGenerator();
 
   Future<List<AccountWithBalance>> obtainAccountsWithBalance() async {
     final accounts = await _accountRepository.findAll();
@@ -17,4 +22,15 @@ class AccountService {
   }
 
   Future<int> saveAccount(Account account) => _accountRepository.createAccount(account);
+
+  Future<AccountKeys> generateAccountKeys() async {
+    final keyPair = await _keyGenerator.generateKeyPair();
+
+    return _accountKeysFrom(keyPair);
+  }
+
+  AccountKeys _accountKeysFrom(KeyPair keyPair) => AccountKeys(
+      hex.encode(keyPair.publicKey),
+      hex.encode(keyPair.secretKey)
+  );
 }
