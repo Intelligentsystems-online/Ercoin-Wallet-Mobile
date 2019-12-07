a81de36a8c4171659f2443a576d08ad938ae1fea
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-07 20:05:41 +0100

    remove static commented method and fix typos in date util

--- lib/utils/DateUtil.dart
+++ lib/utils/DateUtil.dart
@@ -3,11 +3,9 @@ import 'package:intl/intl.dart';
 class DateUtil {
   final _formatter =  DateFormat('yyyy-MM-dd HH:mm:ss');
 
-//  static DateTime dateTimeFrom(int timestampInSeconds) => new DateTime.fromMillisecondsSinceEpoch(timestampInSeconds * 1000);
-
   String dateTimeFrom(int timestampInSeconds) {
-    final datatime = DateTime.fromMillisecondsSinceEpoch(timestampInSeconds * 1000);
+    final datetime = DateTime.fromMillisecondsSinceEpoch(timestampInSeconds * 1000);
 
-    return _formatter.format(datatime);
+    return _formatter.format(datetime);
   }
 }
