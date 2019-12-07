c8cd05a180148043f2f703faaa3c25c0808522dc
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-07 20:03:42 +0100

    add some 'style' for transaction list card

--- lib/utils/DateUtil.dart
+++ lib/utils/DateUtil.dart
@@ -1,3 +1,13 @@
+import 'package:intl/intl.dart';
+
 class DateUtil {
-  static DateTime dateTimeFrom(int timestampInSeconds) => new DateTime.fromMillisecondsSinceEpoch(timestampInSeconds * 1000);
+  final _formatter =  DateFormat('yyyy-MM-dd HH:mm:ss');
+
+//  static DateTime dateTimeFrom(int timestampInSeconds) => new DateTime.fromMillisecondsSinceEpoch(timestampInSeconds * 1000);
+
+  String dateTimeFrom(int timestampInSeconds) {
+    final datatime = DateTime.fromMillisecondsSinceEpoch(timestampInSeconds * 1000);
+
+    return _formatter.format(datatime);
+  }
 }
