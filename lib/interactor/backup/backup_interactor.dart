b33d83ea43d5c963eff76bf763fb6bf1255d3976
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-08 13:09:59 +0100

    add implementation for backup interactor

--- lib/interactor/backup/backup_interactor.dart
+++ lib/interactor/backup/backup_interactor.dart
@@ -1,10 +1,24 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:ercoin_wallet/model/account_keys.dart';
 import 'package:ercoin_wallet/repository/account/Account.dart';
 import 'package:path_provider/path_provider.dart';
 
-// TODO(Interactor)
 class BackupInteractor {
-  Future<String> createBackup(Account account) async { return "mock"; }
+  Future<String> createBackup(Account account) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File(_prepareFilePath(directory.path));
+    final jsonContent = jsonEncode(AccountKeys(account.publicKey, account.privateKey));
+
+    await file.writeAsString(jsonContent);
+
+    return file.path;
+  }
+
+  String _prepareFilePath(String directoryPath) =>
+      directoryPath + "/backup_${_currentTime()}.json";
+
+  String _currentTime() => new DateTime.now().millisecondsSinceEpoch.toString();
 }
