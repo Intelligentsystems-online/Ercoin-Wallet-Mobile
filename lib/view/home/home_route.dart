1502dfe16269076e768255331ca6fb48c9b01f19
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-10 11:31:51 +0100

    remove unused screens

--- lib/view/home/home_route.dart
+++ lib/view/home/home_route.dart
@@ -1,10 +1,7 @@
-import 'package:ercoin_wallet/view/account/AccountScreen.dart';
-import 'package:ercoin_wallet/view/address/AddressScreen.dart';
 import 'package:ercoin_wallet/view/home/page/account_info/account_info_page.dart';
 import 'package:ercoin_wallet/view/home/page/account_list/account_list_page.dart';
 import 'package:ercoin_wallet/view/home/page/address_list/address_list_page.dart';
 import 'package:ercoin_wallet/view/home/page/transaction_list/transaction_list_page.dart';
-import 'package:ercoin_wallet/view/transaction/TransactionScreen.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
