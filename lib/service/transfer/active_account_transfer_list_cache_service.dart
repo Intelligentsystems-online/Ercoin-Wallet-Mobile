c6447b901431c7f68243bbfc081e1fc4757e853f
mpomykalski <mateusz.pomykalski@wext.it>
2019-12-28 21:17:42 +0100

    extract some code to methods

--- lib/service/transfer/active_account_transfer_list_cache_service.dart
+++ lib/service/transfer/active_account_transfer_list_cache_service.dart
@@ -1,3 +1,4 @@
+import 'package:ercoin_wallet/model/base/address.dart';
 import 'package:ercoin_wallet/model/local_account/local_account.dart';
 import 'package:ercoin_wallet/model/transfer/transfer.dart';
 import 'package:ercoin_wallet/service/transfer/api/transfer_api_service.dart';
@@ -39,14 +40,25 @@ class ActiveAccountTransferListCacheService {
   }
 
   Future<List<Transfer>> _updateTransferList(LocalAccount account) async {
-    final inTransferList = await _transferListService.obtainAddressInTransferList(account.namedAddress.address, _lastInPage);
-    final outTransferList = await _transferListService.obtainAddressOutTransferList(account.namedAddress.address, _lastOutPage);
+    final inTransferList = await _fetchInTransfers(account.namedAddress.address, _lastInPage);
+    final outTransferList = await _fetchOutTransfers(account.namedAddress.address, _lastOutPage);
 
-    if(_lastInPage > 1 && inTransferList.length < _minTransfersSize)
-      inTransferList.addAll(await _transferListService.obtainAddressInTransferList(account.namedAddress.address, _lastInPage - 1));
-    if(_lastOutPage > 1 && outTransferList.length < _minTransfersSize)
-      outTransferList.addAll(await _transferListService.obtainAddressOutTransferList(account.namedAddress.address, _lastOutPage - 1));
+    if(_shouldFetchMoreInTransfers(inTransferList.length))
+      inTransferList.addAll(await _fetchInTransfers(account.namedAddress.address, _lastInPage - 1));
+
+    if(_shouldFetchMoreOutTransfers(outTransferList.length))
+      outTransferList.addAll(await _fetchOutTransfers(account.namedAddress.address, _lastOutPage - 1));
 
     return inTransferList + outTransferList;
   }
+
+  Future<List<Transfer>> _fetchInTransfers(Address address, int pageNumber) async =>
+      await _transferListService.obtainAddressInTransferList(address, pageNumber);
+
+  Future<List<Transfer>> _fetchOutTransfers(Address address, int pageNumber) async =>
+      await _transferListService.obtainAddressOutTransferList(address, pageNumber);
+
+  _shouldFetchMoreInTransfers(int transfersAmount) => _lastInPage > 1 && transfersAmount < _minTransfersSize;
+
+  _shouldFetchMoreOutTransfers(int transfersAmount) => _lastOutPage > 1 && transfersAmount < _minTransfersSize;
 }
