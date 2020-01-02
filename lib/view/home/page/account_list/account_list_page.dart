0ce50b86e06fd52e900f4093ad15689ac8edf82e
mpomykalski <mateusz.pomykalski@wext.it>
2020-01-02 12:31:02 +0100

    modify account list view - add activate button and change add account button

--- lib/view/home/page/account_list/account_list_page.dart
+++ lib/view/home/page/account_list/account_list_page.dart
@@ -1,10 +1,12 @@
 import 'package:ercoin_wallet/interactor/account_list/account_list_interactor.dart';
 import 'package:ercoin_wallet/main.dart';
+import 'package:ercoin_wallet/model/local_account/local_account.dart';
 import 'package:ercoin_wallet/model/local_account/local_account_activation_details.dart';
 import 'package:ercoin_wallet/utils/view/account_list.dart';
 import 'package:ercoin_wallet/utils/view/navigation_utils.dart';
 import 'package:ercoin_wallet/utils/view/refreshable_future_builder.dart';
 import 'package:ercoin_wallet/utils/view/searchable_list.dart';
+import 'package:ercoin_wallet/utils/view/top_and_bottom_container.dart';
 import 'package:ercoin_wallet/utils/view/values.dart';
 import 'package:ercoin_wallet/view/add_account/add_account_route.dart';
 import 'package:ercoin_wallet/view/home/home_route.dart';
@@ -23,18 +25,21 @@ class _AccountListPageState extends State<AccountListPage> {
   String _search;
 
   @override
-  Widget build(BuildContext ctx) => Scaffold(
-        body: Container(
-          padding: standardPadding.copyWith(bottom: 0),
-          child: RefreshableFutureBuilder<List<LocalAccountActivationDetails>>(
-            key: _builderKey,
-            forceScrollable: false,
-            futureBuilder: (isRefresh) async => await _interactor.obtainDetailsList(_search, refresh: isRefresh),
-            builder: (_, detailsList) => _accountList(detailsList),
-          ),
-        ),
-        floatingActionButton: _addAccountBtn(ctx),
-      );
+  Widget build(BuildContext ctx) => Container(
+      padding: standardPadding.copyWith(bottom: 0),
+      child: RefreshableFutureBuilder<List<LocalAccountActivationDetails>>(
+        key: _builderKey,
+        forceScrollable: false,
+        futureBuilder: (isRefresh) async => await _interactor.obtainDetailsList(_search, refresh: isRefresh),
+        builder: (_, detailsList) => Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(child: _accountList(detailsList)),
+              _transferBtn(ctx)
+            ],
+        )
+      )
+  );
 
   Widget _accountList(List<LocalAccountActivationDetails> detailsList) => SearchableList(
         onSearchChanged: (value) {
@@ -44,15 +49,21 @@ class _AccountListPageState extends State<AccountListPage> {
         listWidget: AccountList(
           list: detailsList,
           afterBackFromAccountDetails: (_) => _builderKey.currentState.update(isRefresh: true),
+          onActivate: (account) => _onAccountActivation(account)
         ),
       );
 
-  FloatingActionButton _addAccountBtn(BuildContext ctx) => FloatingActionButton(
-        heroTag: "AccountListPage",
-        child: const Icon(Icons.add),
-        onPressed: () => pushRoute(
-          Navigator.of(ctx),
-          () => AddAccountRoute(onAdded: (ctx) => resetRoute(Navigator.of(ctx), () => HomeRoute())),
-        ),
-      );
+  _onAccountActivation(LocalAccount account) {
+    _builderKey.currentState.update(isRefresh: true);
+    _interactor.persistActiveAccountAddress(account);
+  }
+
+  Widget _transferBtn(BuildContext ctx) => RaisedButton.icon(
+    icon: const Text("Create new account"),
+    label: const Icon(Icons.add),
+      onPressed: () => pushRoute(
+        Navigator.of(ctx),
+            () => AddAccountRoute(onAdded: (ctx) => resetRoute(Navigator.of(ctx), () => HomeRoute())),
+      )
+  );
 }
