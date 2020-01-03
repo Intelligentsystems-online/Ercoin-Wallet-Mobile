f1878ce8967ee814ea9565a79ac372c1f2307f14
mpomykalski <mateusz.pomykalski@wext.it>
2020-01-03 22:45:32 +0100

    remove typos from delete recomendation message

--- lib/view/account_details/delete_recomendation_dialog.dart
+++ lib/view/account_details/delete_recomendation_dialog.dart
@@ -22,7 +22,7 @@ class DeleteRecomendationDialog extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text("Before proceed we continue to make sure that you wrote down access keys. Otherwise you can lost account data"),
+          Text("Before proceed make sure that you wrote down access keys. Otherwise you can lost account data"),
           SizedBox(height: 8),
           _buttonsRow(ctx),
         ],
